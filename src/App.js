8ec69ab1d4262d3846da0d0e54bca7fc9b280cdb
rldnjs9347 <rldnjs9347@gmail.com>
2022-09-19 01:14:56 +0900

    docker에 리액트를 빌드

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className='App'>
       <h1>test 1</h1>
+      <h1>test 2</h1>
+      <h1>test 4</h1>
+      <h1>test 5</h1>
+      <h1>test 7</h1>
+      <h1>test 8</h1>
+      <h1>test 9</h1>
+      <h1>test {process.env.REACT_APP_NAME}</h1>
       jenkin + react + docker 테스트
     </div>
   );
